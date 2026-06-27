d074874eede9e5ef8cf411da580931f0a2e5b4f3
Ibnutugas <ibnunazhif4@gmail.com>
2026-06-27 14:31:24 +0700

    Delete app directory

--- app/Models/SupplierModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-use CodeIgniter\Model;
-
-class SupplierModel extends Model {
-    protected $table = 'supplier';
-    protected $primaryKey = 'id';
-    protected $allowedFields = ['nama_supplier', 'no_telepon', 'alamat'];
-}
